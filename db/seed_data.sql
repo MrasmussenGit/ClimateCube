7dd073395377e01fd742566c87953507894b9356
Mike <fusedfoundry@gmail.com>
2026-08-28 12:17:12 -0400

    fix duplicate insert in seed data

--- db/seed_data.sql
+++ db/seed_data.sql
@@ -2,17 +2,6 @@ INSERT INTO room (room_name, description)
 VALUES
 ('Living Room', 'Main living area');
 
-INSERT INTO sensor (
-    device_id,
-    sensor_name,
-    sensor_type
-)
-VALUES (
-    'CC-001',
-    'ClimateCube #1',
-    'BME280'
-);
-
 INSERT INTO room_assignment (
     sensor_id,
     room_id
